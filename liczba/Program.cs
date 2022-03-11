6a891485925a4e0176fce3826ae0b32a4fcbd5ad
Patryk <Patryk@DESKTOP-G97T6KG>
2022-03-11 17:22:38 +0100

    Dodaj pliki projektów.

---
+++ liczba/Program.cs
@@ -0,0 +1,36 @@
+﻿Random r = new Random();
+int num = r.Next(1, 10);
+int guess = 0;
+bool win = false;
+
+Console.WriteLine("Witaj w grze pod tytułem zgadnij liczbe!");
+Console.WriteLine("Wylosowałem liczbę z przedziału od 1 do 10.");
+Console.WriteLine("Wprowadz liczbe:");
+
+while (!win)
+{
+    string read = Console.ReadLine();
+    bool ok = int.TryParse(read, out guess);
+    if (!ok)
+    {
+        Console.WriteLine("Wprowadzona wartośc nie jest liczbą.");
+        continue;
+    }
+    if (guess < 1 || guess > 10)
+    {
+        Console.WriteLine("Wprowadzona liczba nie mieści się w zakresie! Spróbuj ponownie.");
+    }
+    else if (guess < num)
+    {
+        Console.WriteLine("Wylosowana liczba jest większa od podanej");
+    }
+    else if (guess > num)
+    {
+        Console.WriteLine("Wylosowana liczba jest mniejsza od podanej");
+    }
+    else
+    {
+        Console.WriteLine("Wygrałeś!");
+        break;
+    }
+}
